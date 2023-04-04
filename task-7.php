96f5022d7bb415ce4392e64a650ac644136549ee
pashaderalte <pstarostin25@gmail.com>
2023-04-04 22:18:26 +0300

    getFirstFibonacci() in task-7.php implementation changed

--- task-7.php
+++ task-7.php
@@ -1,18 +1,11 @@
 <?php
-    function getFirstFibonacciByLength (int $length): int
+    function getFirstFibonacci(int $length): int
     {
-        $n1 = 0;
-        $n2 = 1;
-
-        if ($length != 1)
-        {
-            while (strlen((string) $n1) <= $length)
-            {
-                $nth = $n1 + $n2;
-                $n1 = $n2;
-                $n2 = $nth;
-            }
+        $fibonacci = [0, 1];
+        $i = 1;
+        while (strlen($fibonacci[$i]) < $length) {
+            $i++;
+            $fibonacci[$i] = bcadd($fibonacci[$i-1], $fibonacci[$i-2]);
         }
-
-        return $n1;
+        return $fibonacci[$i];
     }
