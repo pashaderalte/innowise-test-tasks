5d0dc1de493bbe4c89fa16ff4404f34850eb710e
pashaderalte <pstarostin25@gmail.com>
2023-04-11 13:13:45 +0300

    I hope the last one 4real🥺

--- task-7.php
+++ task-7.php
@@ -1,11 +1,40 @@
 <?php
-    function getFirstFibonacci(int $length): int
+    function getFirstFibonacci(int $length): string
     {
         $fibonacci = [0, 1];
         $i = 1;
         while (strlen($fibonacci[$i]) < $length) {
             $i++;
-            $fibonacci[$i] = bcadd($fibonacci[$i-1], $fibonacci[$i-2]);
+            $fibonacci[$i] = notBCAdd($fibonacci[$i-1], $fibonacci[$i-2]);
         }
         return $fibonacci[$i];
     }
+
+    function notBCAdd(string $a, string $b): string
+    {
+        while (strlen($a) < strlen($b)) {
+            $a = "0" . $a;
+        }
+        while (strlen($b) < strlen($a)) {
+            $b = "0" . $b;
+        }
+        $result = "";
+        $remainder = 0;
+        for ($i = strlen($a)-1; $i >= 0; $i--) {
+            $digit = intval($a[$i]) + intval($b[$i]) + $remainder;
+            $remainder = 0;
+            if ($digit >= 10) {
+                $digit -= 10;
+                $remainder = 1;
+            }
+            $result = $digit . $result;
+        }
+        if ($remainder > 0) {
+            $result = $remainder . $result;
+        }
+        return $result;
+    }
+
+
+
+
